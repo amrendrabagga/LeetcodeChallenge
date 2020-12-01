2b0e9992ff6b48bd29bfcff41d3fc90aa3141944
Amrendra Bagga <amrendrabagga@gmail.com>
2020-12-02 00:38:05 +0530

    december week1 day1 maximum depth of tree

---
+++ src/main/java/december/week1/Day1_MaximumDepthOfBinaryTree.java
@@ -0,0 +1,12 @@
+package december.week1;
+
+import utils.TreeNode;
+
+public class Day1_MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return 1 + Math.max(left, right);
+    }
+}
